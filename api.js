247f6aa8f0ce3dcf75c52801bc712f308fe9438d
ithirzty <45016834+ithirzty@users.noreply.github.com>
2020-07-14 15:59:16 +0200

    api.js
    https://cdn.alois.xyz/api.js

---
+++ api.js
@@ -0,0 +1,31 @@
+////////LIVE - ALOIS ///// API.JS /////////
+//////////////COPYRIGHT ALOIS LAURENT BOE//
+///////////////////////////////////////////
+/// <reference path="https://code.jquery.com/jquery-3.4.1.min.js" />
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+function getChatMessages(name, callback) {
+  var evtSource = new EventSource("https://live.alois.xyz/api/?GetChatMessages="+name);
+  evtSource.onmessage = function(e) {
+    item = JSON.parse(e.data);
+        if(item.m != undefined) {
+          callback({'pseudo': item.p, 'message': item.m, 'userColor': item.c, 'messageColor': item.mc, 'time': item.t});
+      }
+  };
+}
+
+function sendChatMessage(message, key) {
+  $.get("https://live.alois.xyz/api/?SendChatMessage="+message+"&key="+key, function(e) {
+    return JSON.parse(e);
+  });
+}
+
+function getStreams(limit, callback) {
+  $.get("https://live.alois.xyz/api/?GetStreams="+limit, function(e) {
+    callback(Json.parse(e));
+  });
+}
