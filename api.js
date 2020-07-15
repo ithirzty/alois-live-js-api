fcc5761f3e840740d46c92a92b09d0a961d8f964
ithirzty <45016834+ithirzty@users.noreply.github.com>
2020-07-15 17:25:27 +0200

    Update api.js
    Updating the connection reset handler in getChatMessages()

--- api.js
+++ api.js
@@ -6,15 +6,20 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
-
+var getChatMessageslasttime = 0;
 
 function getChatMessages(name, callback) {
-  var evtSource = new EventSource("https://live.alois.xyz/api/?GetChatMessages="+name);
+  var evtSource = new EventSource("https://live.alois.xyz/api/?GetChatMessages="+name+"&lasttime="+getChatMessageslasttime);
   evtSource.onmessage = function(e) {
     item = JSON.parse(e.data);
         if(item.m != undefined) {
+          getChatMessageslasttime = item.t;
           callback({'pseudo': item.p, 'message': item.m, 'userColor': item.c, 'messageColor': item.mc, 'time': item.t});
       }
+  };
+  evtSource.onerror = function() {
+    evtSource.close();
+    evtSource = new EventSource("https://live.alois.xyz/api/?GetChatMessages="+name+"&lasttime="+getChatMessageslasttime);
   };
 }
 
